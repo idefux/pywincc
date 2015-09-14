a70a2638c4f87be0830e8c62cd9fe882e0748df5
Idefux <fuchs.stefan@gmx.at>
2015-09-14 16:53:30 +0200

    Bugfix and improvements in alarm list filtering.

--- reports/_out/alarms.js
+++ reports/_out/alarms.js
@@ -1,39 +1,30 @@
 function alarm_filter() {
-    if (document.getElementById('check_come').checked == 1) {
-        $(".alCOME").removeClass("hidden-row");
-    } else {
+    // Make all rows visible
+    $(".alCOME").removeClass("hidden-row");
+    $(".alGO").removeClass("hidden-row");
+    $(".alACK").removeClass("hidden-row");
+    $(".alGACK").removeClass("hidden-row");
+    // Hide rows based on settings
+    if (document.getElementById('check_come').checked == 0) {
         $(".alCOME").addClass("hidden-row");
     }
-    if (document.getElementById('check_go').checked == 1) {
-        $(".alGO").removeClass("hidden-row");
-    } else {
+    if (document.getElementById('check_go').checked == 0) {
         $(".alGO").addClass("hidden-row");
     }
-    if (document.getElementById('check_ack_gack').checked == 1) {
-        $(".alACK").removeClass("hidden-row");
-        $(".alGACK").removeClass("hidden-row");
-    } else {
+    if (document.getElementById('check_ack_gack').checked == 0) {
         $(".alACK").addClass("hidden-row");
         $(".alGACK").addClass("hidden-row");
     }
-    if (document.getElementById('check_warning').checked == 1) {
-        $(".alWARNING").removeClass("hidden-row");
-    } else {
+    if (document.getElementById('check_warning').checked == 0) {
         $(".alWARNING").addClass("hidden-row");
     }
-    if (document.getElementById('check_error_day').checked == 1) {
-        $(".alERROR_DAY").removeClass("hidden-row");
-    } else {
+    if (document.getElementById('check_error_day').checked == 0) {
         $(".alERROR_DAY").addClass("hidden-row");
     }
-    if (document.getElementById('check_error_now').checked == 1) {
-        $(".alERROR_NOW").removeClass("hidden-row");
-    } else {
+    if (document.getElementById('check_error_now').checked == 0) {
         $(".alERROR_NOW").addClass("hidden-row");
     }
-    if (document.getElementById('check_stop_all').checked == 1) {
-        $(".alSTOP_ALL").removeClass("hidden-row");
-    } else {
+    if (document.getElementById('check_stop_all').checked == 0) {
         $(".alSTOP_ALL").addClass("hidden-row");
     }
 }
