2ca18e63c6424e67f04e4f016a2199f909102b5b
Stefan Fuchs <fuchs.stefan@gmx.at>
2016-02-18 15:36:45 +0100

    Fix entry points; add LICENSE.md

--- setup.py
+++ setup.py
@@ -17,16 +17,13 @@ setup(
     install_requires=install_requires,
     name='pywincc',
     version=__version__,
-    packages=find_packages(exclude=['docs', 'reports', 'venv', 'tests'])
-#   py_modules=['pywincc', 'alarm', 'helper', 'interactive', 'mssql',
-#               'operator_messages', 'parameter', 'report', 'tag',
-#               'wincc_hosts', 'wincc'],
-    ,
-    entry_points='''
-        [console_scripts]
-        pywincc=pywincc.pywincc:cli
-        wincc_hosts=pywincc.wincc_hosts:cli
-    ''',
+    packages=find_packages(exclude=['docs', 'reports', 'venv', 'venv2', 'tests']),
+    entry_points={
+              'console_scripts': [
+                                  'pywincc = pywincc.wincc_connect:cli',
+                                  'wincc_hosts = pywincc.wincc_hosts:cli'
+                                  ],
+              },
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
